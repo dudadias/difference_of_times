ffa2ef1e2fe0624f85da7c2a3f51d921c34d6d09
dudadiass <diasnights@gmail.com>
2023-06-04 23:46:29 -0300

    six integers scanner

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+
+/*
+    Find how many seconds passed between the two moments of time 
+    on the same day.
+
+    You are given the integer values representing these moments:
+    hours, minutes and seconds for each of them.
+    It is guaranteed that the earlier moment goes first in the input.
+ */
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt(); // hour
+        int number2 = scanner.nextInt(); // minute
+        int number3 = scanner.nextInt(); // second
+        int number4 = scanner.nextInt();
+        int number5 = scanner.nextInt();
+        int number6 = scanner.nextInt();
     }
 }
